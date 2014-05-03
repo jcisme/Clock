6c643e0e4bd851681350fec1b8c3e1cf6a40c2f9
jcisme <john.andrew.coffin@gmail.com>
2014-05-02 22:08:09 -0600

    Clean-up Code
    Get rid of parts of code not in use.

--- app/src/main/java/com/jcisme/clock/app/MainActivity.java
+++ app/src/main/java/com/jcisme/clock/app/MainActivity.java
@@ -7,8 +7,6 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.text.format.DateFormat;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 
 import java.util.Date;
@@ -41,25 +39,6 @@ public class MainActivity extends ActionBarActivity {
 
         unregisterReceiver(clock);
         clock = null;
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     private class TimeBroadcastReceiver extends BroadcastReceiver {
