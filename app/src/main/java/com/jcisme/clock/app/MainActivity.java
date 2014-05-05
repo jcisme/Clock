4988ac5081d89bc4acb81634d119e79de22680a5
jcisme <john.andrew.coffin@gmail.com>
2014-05-04 22:23:57 -0600

    Add Sound and Animation
    Not going well at all. May want to revert this once we get it all
    working. Sigh.

--- app/src/main/java/com/jcisme/clock/app/MainActivity.java
+++ app/src/main/java/com/jcisme/clock/app/MainActivity.java
@@ -4,24 +4,31 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.text.format.DateFormat;
-import android.widget.TextView;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 
-import java.util.Date;
-
+import java.util.Calendar;
 
 public class MainActivity extends ActionBarActivity {
 
-    private TextView mTextView;
-    private TimeBroadcastReceiver mTimeBroadcastReceiver = new TimeBroadcastReceiver();;
+    private int mCurrentHour;
+    private int mCurrentMinute;
+    private ImageView mImageViewHourHand;
+    private ImageView mImageViewMinuteHand;
+    private TimeBroadcastReceiver mTimeBroadcastReceiver = new TimeBroadcastReceiver();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTextView = (TextView)findViewById(R.id.textView);
+        mImageViewHourHand = (ImageView) findViewById(R.id.imageViewHourHand);
+        mImageViewMinuteHand = (ImageView) findViewById(R.id.imageViewMinuteHand);
+        mCurrentHour = 0;
+        mCurrentMinute = 0;
     }
 
     @Override
@@ -38,10 +45,97 @@ public class MainActivity extends ActionBarActivity {
     }
 
     protected void updateDisplay() {
-        Date date = new Date();
-        CharSequence time = DateFormat.format("hh:mm a", date.getTime());
-        time = time.toString().toUpperCase();
-        mTextView.setText(time);
+        advanceMinuteHand();
+//        advanceHourHand(newHour);
+    }
+
+    protected void advanceHourHand() {
+        Calendar dateTime = Calendar.getInstance();
+        int newHour =  dateTime.get(Calendar.HOUR);
+        newHour = newHour  % 12;
+        if (newHour == mCurrentHour) return;
+
+        float startAngle = mCurrentHour * 30f;
+        float endAngle = (mCurrentHour + 1) * 30f;
+        RotateAnimation moveHourHandAnimation = new RotateAnimation(startAngle, endAngle,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        moveHourHandAnimation.setDuration(100);
+        moveHourHandAnimation.setFillAfter(true);
+        moveHourHandAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                //do nothing
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                //playHourUpdateSound();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+                //do nothing
+            }
+        });
+        mImageViewHourHand.setAnimation(moveHourHandAnimation);
+
+        mCurrentHour = (mCurrentHour + 1) % 12;
+    }
+
+    private void playHourUpdateSound() {
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.clong);
+        mediaPlayer.start();
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                mediaPlayer.release();
+            }
+        });
+    }
+
+    protected void advanceMinuteHand() {
+        Calendar dateTime = Calendar.getInstance();
+        int newMinute =  dateTime.get(Calendar.MINUTE);
+        newMinute = (60 + newMinute) % 60;
+        if (newMinute == mCurrentMinute) return;
+
+        float startAngle = mCurrentMinute * 6f;
+        float endAngle = (mCurrentMinute + 1) * 6f;
+        RotateAnimation moveMinuteHandAnimation = new RotateAnimation(startAngle, endAngle,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        moveMinuteHandAnimation.setDuration(100);
+        moveMinuteHandAnimation.setFillAfter(true);
+        moveMinuteHandAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                //do nothing
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                playUpdateMinuteSound();
+                advanceMinuteHand();
+             }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+                //do nothing
+            }
+        });
+        mImageViewMinuteHand.setAnimation(moveMinuteHandAnimation);
+
+        mCurrentMinute = (mCurrentMinute + 1) % 60;
+    }
+
+    private void playUpdateMinuteSound() {
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.mechanical_clonk);
+        mediaPlayer.start();
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                mediaPlayer.release();
+            }
+        });
     }
 
     private class TimeBroadcastReceiver extends BroadcastReceiver {
@@ -49,7 +143,7 @@ public class MainActivity extends ActionBarActivity {
         @Override
         public void onReceive(Context context, Intent intent) {
             if (Intent.ACTION_TIME_TICK.equalsIgnoreCase(intent.getAction())) {
-                updateDisplay();
+                MainActivity.this.updateDisplay();
             }
         }
     }
